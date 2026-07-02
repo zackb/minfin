bb1d52c68209b0141448230b6b1b686009472a22
Zack Bartel <zack@bartel.com>
2026-07-01 17:06:42 -0700

    brew: bump formula to v0.1.4

--- Formula/minfin.rb
+++ Formula/minfin.rb
@@ -1,8 +1,8 @@
 class Minfin < Formula
   desc "Personal finance app that syncs SimpleFIN accounts into local SQLite"
   homepage "https://github.com/zackb/minfin"
-  url "https://github.com/zackb/minfin/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "142f237a1c42f353b26aa98db00ae6f75cb48a3b5b7ac276bf2456067e8a2611"
+  url "https://github.com/zackb/minfin/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "5ec9f2af238050db8b57cd63d9f29b0e64aea175243f365e40e42ec5d80ee124"
   license "MIT"
   head "https://github.com/zackb/minfin.git", branch: "main"
 
